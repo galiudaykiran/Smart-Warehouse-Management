90abcdfe837600d6bf4d4c50329df280337c72e8
galiudaykiran <udaykirangali2@gmail.com>
2026-07-23 11:31:24 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/smart_warehouse_management/SmartWarehouseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.smart_warehouse_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartWarehouseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartWarehouseManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smart_warehouse_management/SmartWarehouseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_warehouse_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartWarehouseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
